48666e7a87d345517ecaab3911dfe2f74c3f5a2f
João Paulo S. Costa <43136205+kjuao9@users.noreply.github.com>
2019-06-18 23:26:06 -0300

    pinguim pulante do ártico

---
+++ site/includes/menu.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="pt-Br">
+<head>
+<meta charset="utf-8">
+<meta author = "João Paulo S. Costa">
+</head>
+<body>
+<div id="menufixo">
+<a href="index.php" class = "centralizar">Index</a>
+<a></a>
+<a></a>
+</div>
+</body>
+</html>
---
+++ site/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<?php
+require "includes/menu.php"; 
+?>
+<html lang="pt-Br">
+<head>
+<meta charset="utf-8">
+<meta author = "João Paulo S. Costa">
+<meta keywords = "Queijo, Cheese, Queijito">
+<meta description = "Tudo Sobre Queijos!">
+<title>Meu Site</title>
+<link rel="stylesheet" href="css/estilo.css" />
+<link rel="shortcut icon" href="imagens/phpThumb_generated_thumbnailico" />
+</head>
+<style>
+body{
+     background-image: url("imagens/Penguins.jpg");
+      background-position: center;
+}
+</style>
+
+<script>
+</script>
+<body>
+<header>
+<h2>Titulo<h2>
+</header>
+<nav>
+<ul>
+<li>ads</li>
+
+</ul>
+</nav>
+<footer>
+<h5>&copyCopyright: JoãoP & Cia<sup>&#174</sup>. Todos os direitos reservados.</h5>
+</footer>
+</body>
+</html>
